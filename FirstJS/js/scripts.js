9181f54205cb3252d0153c2d1994088f0ba583c5
SergeyRyzhikh <serhii.ryzhykh@nure.ua>
2021-04-13 16:03:38 +0300

    test

--- FirstJS/js/scripts.js
+++ FirstJS/js/scripts.js
@@ -1,1 +1,21 @@
-alert('Hello World')
+function isNumberOdd(number){
+	var x = null;
+	number % 2 == 1 ? x = true: x = false;
+	return x;
+}
+
+function factorial(number){
+	var temp = 1;
+	for(i = 1; i <= number; i++){
+		temp *= i;
+	}
+	return temp;
+}
+
+function changeSpaceToUnderscore(string){
+	string = string.replace(/ /g,"_");
+	return string;	
+}
+console.log(isNumberOdd(23));
+console.log(factorial(0));
+console.log(changeSpaceToUnderscore("create function"))
