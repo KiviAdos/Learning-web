5d7425c66fbdee4a43ad28ba94264f0d13bc11c9
SergeyRyzhikh <serhii.ryzhykh@nure.ua>
2021-04-16 23:06:10 +0300

    finishing choose your price

--- FirstJS/js/scripts.js
+++ FirstJS/js/scripts.js
@@ -1,20 +1,4 @@
-var carSeller1 = {
-	firstName: "Jack",
-	lastName: "White",
-	regYear: 2017,
-	hasDiscount: true,
-	discountCalculation: function() {
-		var discount;
-		var numberOfYears = 2021 - this.regYear;
-		if(numberOfYears <= 2) {
-			discount = 0;
-		} else if((numberOfYears > 2) && (numberOfYears <= 5)) {
-			discount = 20;
-		} else if(numberOfYears > 5) {
-			discount = 30;
-		}
-		return discount;
-	}
-}
+document.getElementById("header").innerHTML = "New vlaue";
+var y = document.getElementsByClassName("list");
 
-console.log(carSeller1.discountCalculation());
+console.log(y);
